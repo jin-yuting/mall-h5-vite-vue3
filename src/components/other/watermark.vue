de25bd6c3fa784e64cd624500e4709497b2b54fc
jinyt <610212587@qq.com>
2023-06-28 10:58:48 +0800

    fix:水印

--- src/components/other/watermark.vue
+++ src/components/other/watermark.vue
@@ -5,24 +5,22 @@
 <script setup>
 import { ref, onMounted } from "vue";
 const waterMark = ref(null);
+
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
-
-canvas.width = 200
-canvas.height = 100
-// ctx.font = '18px Vedana';
-// ctx.fillStyle = 'rgba(200,200,200,0.4)'
 ctx.rotate(-20*Math.PI / 180)
 ctx.textAlign = 'left'
 ctx.fillText('我是水印', canvas.width/16, canvas.height/2)
 
+canvas.width = 200
+canvas.height = 100
+
+document.body.appendChild(canvas);
 
 
 onMounted(() => {
   waterMark.value.style.backgroundRepeat = 'repeat';
-  waterMark.value.style.width = document.documentElement.clientWidth + 'px';
-  waterMark.value.style.height = document.documentElement.clientHeight + 'px';
-  
-  document.body.appendChild(canvas);
+  // waterMark.value.style.width = document.documentElement.clientWidth + 'px';
+  // waterMark.value.style.height = document.documentElement.clientHeight + 'px';
 });
 </script>
